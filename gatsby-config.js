e3de5416dbf014144c1febe82a1984c2d99410b5
cafebabe007 <shilovana@gmail.com>
2020-03-17 16:24:53 -0700

    chore: adding images to static

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `John Smilga`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Anna Shilova`,
+    description: `Professional developer site portfolio built with ReactJs.`,
+    author: `Anna Shilova`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
