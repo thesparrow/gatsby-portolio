085abd16c9807d239e2db0086d0820e838ba2c38
cafebabe007 <shilovana@gmail.com>
2020-09-28 15:24:12 -0700

    feature: adding animation using Typing

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,18 @@
 import React from "react"
+import Typing from "react-typing-animation"
+
 import styles from "../css/banner.module.css"
 
 const Banner = ({ title, children }) => {
   return (
     <div className={styles.banner}>
-      <h1 className={styles.title}>{title}</h1>
+      <Typing
+        speed={90}
+        startDelay={1000}>
+        <h1 className={styles.title}>{title}</h1>
+      </Typing>
       {children}
+      <span>Let's build your digital business together</span>
     </div>
   )
 }
